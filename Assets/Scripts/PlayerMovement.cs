6277e1d8d04289846ff60496a2df48b739089880
Martin <martin.allenstein@gmail.com>
2025-04-29 01:18:31 +0700

    Add Scripts, Tilemap
    Add Scripts
    - movement
    - camera follow

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,10 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb2d;
 
-    Vector2 moveInput; //For walk with Addforce
-    [SerializeField] private float speed;
-
-    [SerializeField]float jumpForce;
-    [SerializeField]bool isJumping;
+    Vector2 moveInput;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private bool isJumping;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -17,15 +16,21 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         //Walk
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        rb2d.AddForce(moveInput * speed);        
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
         
         //Jump
         if (Input.GetButtonDown("Jump") && !isJumping)
         {
-            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+            rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpForce);
+            //rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             //Debug.Log("Jump!");
         }
+    }
+
+    void FixedUpdate()
+    {
+        //rb2d.AddForce(moveInput * speed);
+        rb2d.linearVelocity = new Vector2(moveInput.x * speed, rb2d.linearVelocity.y);
     }
     
     private void OnCollisionEnter2D(Collision2D other)
